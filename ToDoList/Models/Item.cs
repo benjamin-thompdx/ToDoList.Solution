4c4991e2c7df4438519655070615a1443fb92c64
Benjamin Thom <jaminthom@gmail.com>
2020-03-15 17:52:28 -0700

    remove all references to _instances from ToDoList/Models/Item.cs

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,28 +6,21 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Item> _instances = new List<Item> {};
 
-    public Item (string description)
+    public Item(string description)
     {
-      Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
-    }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
+        Description = description;
     }
 
     public static void ClearAll()
     {
-      _instances.Clear();
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId-1];
+      // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
     }
   }
 }
