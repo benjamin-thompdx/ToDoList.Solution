be1fcdfad6cd9f140effe96f6ec538fc596e8b42
Benjamin Thom <jaminthom@gmail.com>
2020-03-23 08:04:46 -0700

    added all necessary CRUD functionality to our ItemsController with the exception of delete functionality, add a form to our Items/Details.cshtml view to handle the POST request necessary to delete the entry, create the corresponding POST route in the items controller

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -18,41 +18,41 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-        return View(_db.Items.ToList());
+      return View(_db.Items.ToList());
     }
 
     public ActionResult Create()
     {
-        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-        return View();
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
     }
 
     [HttpPost]
     public ActionResult Create(Item item, int CategoryId)
     {
-        _db.Items.Add(item);
-        if (CategoryId != 0)
-        {
-            _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
-        }
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+      _db.Items.Add(item);
+      if (CategoryId != 0)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-        var thisItem = _db.Items
-            .Include(item => item.Categories)
-            .ThenInclude(join => join.Category)
-            .FirstOrDefault(item => item.ItemId == id);
-        return View(thisItem);
+      var thisItem = _db.Items
+          .Include(item => item.Categories)
+          .ThenInclude(join => join.Category)
+          .FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
     }
 
     public ActionResult Edit(int id)
     {
-        var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-        return View(thisItem);
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
     }
 
     [HttpPost]
@@ -77,27 +77,36 @@ namespace ToDoList.Controllers
     [HttpPost]
     public ActionResult AddCategory(Item item, int CategoryId)
     {
-        if (CategoryId != 0)
-        {
+      if (CategoryId != 0)
+      {
         _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
-        }
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
-//     public ActionResult Delete(int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       return View(thisItem);
-//     }
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
 
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       _db.Items.Remove(thisItem);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteCategory(int joinId)
+    {
+      var joinEntry = _db.CategoryItem.FirstOrDefault(entry => entry.CategoryItemId == joinId);
+      _db.CategoryItem.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
