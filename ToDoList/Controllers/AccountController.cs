2de150a3b6c9fbd88a20ea73c80c843f02a24def
Benjamin Thom <jaminthom@gmail.com>
2020-03-24 07:33:28 -0700

    check ToDoList/Controllers/AccountController.cs for errors

--- ToDoList/Controllers/AccountController.cs
+++ ToDoList/Controllers/AccountController.cs
@@ -1,43 +1,44 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using ToDoList.Models;
+using System.Threading.Tasks;
 using ToDoList.ViewModels;
 
-namespace ToDoList.Controllers 
+namespace ToDoList.Controllers
 {
-  public class AccountController : Controller 
+  public class AccountController : Controller
   {
     private readonly ToDoListContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db) 
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
     {
       _userManager = userManager;
       _signInManager = signInManager;
       _db = db;
     }
 
-    public ActionResult Index() 
+    public ActionResult Index()
     {
-      return View ();
+      return View();
     }
 
-    public IActionResult Register() 
+    public IActionResult Register()
     {
-      return View ();
+      return View();
     }
 
     [HttpPost]
-    public async Task<ActionResult> Register (RegisterViewModel model) {
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
       var user = new ApplicationUser { UserName = model.Email };
-      IdentityResult result = await _userManager.CreateAsync (user, model.Password);
-      if (result.Succeeded) 
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
       {
-        return RedirectToAction ("Index");
-      } 
-      else 
+        return RedirectToAction("Index");
+      }
+      else
       {
         return View();
       }
